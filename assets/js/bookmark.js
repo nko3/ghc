588b5eeadcc03adc3230b3d21cff7579d8d12a6c
austin <austin@rawfingertips.com>
2012-11-11 16:38:24 -0800

    fix bookmark typo

--- assets/js/bookmark.js
+++ assets/js/bookmark.js
@@ -21,9 +21,8 @@
     return $("#githubchat-frame").attr('src', 'http://githubchat.us/chats/fighting-mongeese/popup').css({
       position: "fixed",
       right: 0,
-      top: 100
-    }, z - {
-      index: 2000
+      top: 100,
+      'z-index': 2000
     });
   };
 
